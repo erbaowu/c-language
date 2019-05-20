6ed64a4c1c4ab892a3f808b971774dec3d1a48fe
erbaowu <web0528@126.com>
2019-05-20 16:24:06 +0800

    update code3

---
+++ test1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/*int main(void)
+{
+int height=8, length=12, width=10, volume=height*length*width, weight=(volume+165)/166;
+
+printf("Dimensions:%dx%dx%d\n",length,width,height);
+printf("Volume(cubic inches):%d\n",volume);
+printf("Dimensional weight(pounds):%d\n",(volume+165)/166);
+
+return 0;
+}*/
+/*
+float main(void)
+{
+#define PI 3.1415926535f
+int r;
+printf("请输入球体的半径：");
+scanf("%d",&r);
+printf("所求的的球体的体积为：%f",(4.0f/3.0f)*PI*r*r*r);
+ 
+}
+*/
+ /* float main(void)
+{
+	float raw_input;
+	printf("Enter an amount:");
+	scanf("%f",&raw_input);
+	printf("with tax added:%.2f",raw_input*(1.05f));
+}
+*/
+/*
+float main(void)
+{float x,a;
+printf("请输入x的值；");
+scanf("%f",&x);
+a=3*x*x*x*x*x+2*x*x*x*x-5*x*x*x-x*x+7*x-6;
+printf("%.2f",a) ;
+	
+}*/
